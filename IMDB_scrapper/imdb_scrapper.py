93069f754479edcabf535d8745c1b4ad94653710
Aditya Jetely <ajetely@gmail.com>
2021-05-09 13:24:09 +0530

    Reformatted

--- IMDB_scrapper/imdb_scrapper.py
+++ IMDB_scrapper/imdb_scrapper.py
@@ -8,86 +8,86 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-l", "--movie", required=True, help="Movie Name")
 args = vars(parser.parse_args())
 
-#Base IMBD URL to search for movie titles
+# Base IMBD URL to search for movie titles
 IMDB_URL = "https://www.imdb.com/search/title/?title="
 
-#Movie the user wants to search for
-USER_DEFINED_MOVIE = args['movie']
+# Movie the user wants to search for
+USER_DEFINED_MOVIE = args["movie"]
 
-#To handle connection error
+# To handle connection error
 try:
-    response = requests.get(IMDB_URL+USER_DEFINED_MOVIE)
+    response = requests.get(IMDB_URL + USER_DEFINED_MOVIE)
 
 except requests.exceptions.ConnectionError as error:
     sys.exit("Check your connection!")
 
-#Creating a soup object
-soup = bs(response.content, 'html.parser')
+# Creating a soup object
+soup = bs(response.content, "html.parser")
 
-#Function to scrap the details about the movie, set n/a if some detail is missing and store the detail into a dictionary
+# Function to scrap the details about the movie, set n/a if some detail is missing and store the detail into a dictionary
 def scrap_and_store(soup):
-    
-    #This dictionary stores the movie information
+
+    # This dictionary stores the movie information
     movie_info = {}
-    
-    #Try and except blocks to ensure correct data retrival
+
+    # Try and except blocks to ensure correct data retrival
     try:
         movie = soup.select(".lister-item-content")[0]
     except:
         movie = "N/A"
-    
+
     if movie == "N/A":
         sys.exit("Movie not found in IMDB")
 
     try:
-        movie_info['Name'] = movie.find("a").contents[0]
+        movie_info["Name"] = movie.find("a").contents[0]
     except:
-        movie_info['Name'] = "N/A"
+        movie_info["Name"] = "N/A"
     try:
-        movie_info['Rating'] = movie.select(".value")[0].contents[0]
+        movie_info["Rating"] = movie.select(".value")[0].contents[0]
     except:
-        movie_info['Rating'] = "N/A"
+        movie_info["Rating"] = "N/A"
     try:
-        movie_info['Released'] = movie.find_all("span")[1].contents[0][1:-1]
+        movie_info["Released"] = movie.find_all("span")[1].contents[0][1:-1]
     except:
-        movie_info['Released'] = "N/A"
+        movie_info["Released"] = "N/A"
     try:
-        movie_info['Certificate'] = movie.select(".certificate")[0].contents[0]
+        movie_info["Certificate"] = movie.select(".certificate")[0].contents[0]
     except:
-        movie_info['Certificate'] = "N/A"
+        movie_info["Certificate"] = "N/A"
     try:
-        movie_info['Runtime'] = movie.select(".runtime")[0].contents[0]
+        movie_info["Runtime"] = movie.select(".runtime")[0].contents[0]
     except:
-        movie_info['Runtime'] = "N/A"
+        movie_info["Runtime"] = "N/A"
     try:
-        movie_info['Genre'] = movie.select(".genre")[0].contents[0].strip()
-    except :
-        movie_info['Genre'] = "N/A"
-    try:    
-        movie_info['Summary'] = movie.select(".text-muted")[2].contents[0].strip()
+        movie_info["Genre"] = movie.select(".genre")[0].contents[0].strip()
     except:
-        movie_info['Summary'] = "N/A"
-    try:    
-        movie_info['Director'] = movie.find_all('p')[2].find_all("a")[0].contents[0]
+        movie_info["Genre"] = "N/A"
+    try:
+        movie_info["Summary"] = movie.select(".text-muted")[2].contents[0].strip()
     except:
-        movie_info['Director'] = "N/A"
+        movie_info["Summary"] = "N/A"
+    try:
+        movie_info["Director"] = movie.find_all("p")[2].find_all("a")[0].contents[0]
+    except:
+        movie_info["Director"] = "N/A"
 
     try:
-        cast_members = movie.find_all('p')[2].find_all("a")[1:]
+        cast_members = movie.find_all("p")[2].find_all("a")[1:]
     except:
         cast_members = []
 
     cast_name = ""
     for member in cast_members:
-        cast_name+=member.contents[0]+", "
+        cast_name += member.contents[0] + ", "
+
+    movie_info["Cast"] = cast_name[:-2]
+    return movie_info
 
 
-    movie_info['Cast'] = cast_name[:-2]
-    return movie_info    
-
-#This function fetches the movie information and prints it systematically
+# This function fetches the movie information and prints it systematically
 def main():
 
     info = scrape_and_store(soup)
-    for key,value in info.items():
+    for key, value in info.items():
         print(f"{key} : {value}")
--- Wallpaper Setter/wallpaper_setter.py
+++ Wallpaper Setter/wallpaper_setter.py
@@ -9,25 +9,25 @@ parser.add_argument("-l", "--jpgfile", required=True, help="JPG File")
 args = vars(parser.parse_args())
 
 
-#Storing the absolute path of the jpg file provided by the user
-filename = args['jpgfile']
+# Storing the absolute path of the jpg file provided by the user
+filename = args["jpgfile"]
 
 # To make sure a jpg file has been provided
 if filename.endswith(".jpg"):
-    
-
 
     # Code to get the absolute path of the jpg file
     def getabsolutepath(filename):
         path = os.path.abspath(filename)
         return path
 
-    #The code for setting wallpaper in SystemParametersInfoW function is 20 
-    SPI_SETDESKWALLPAPER = 20 
+    # The code for setting wallpaper in SystemParametersInfoW function is 20
+    SPI_SETDESKWALLPAPER = 20
 
-    #Main code to change the wallpaper
-    output_from_wallpaper_change = ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, getabsolutepath(filename), 0) 
-    
+    # Main code to change the wallpaper
+    output_from_wallpaper_change = ctypes.windll.user32.SystemParametersInfoW(
+        SPI_SETDESKWALLPAPER, 0, getabsolutepath(filename), 0
+    )
+
     if output_from_wallpaper_change == 1:
         print("Your wallpaper has been changed successfully!!!")
     else:
